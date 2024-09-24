d93bbd997333499c9a19e2f6dc589225d68ac258
variyarudrax <rudrax.variya@bacancy.com>
2024-09-24 18:50:31 +0530

    fix(bug fix): bug fix

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,18 @@ import { NextRequest, NextResponse } from "next/server"
 import { getToken } from "next-auth/jwt"
 
 export async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname
   const secret = process.env.NEXTAUTH_SECRET
   if (!secret) {
     throw new Error("NEXTAUTH_SECRET is not defined")
   }
   const session = await getToken({ req, secret })
-
-  if (!session && path !== "/login") {
+  const url =
+    process.env.NODE_ENV === "production"
+      ? "https://next-tech-showcase-iota.vercel.app/"
+      : "http://localhost:3000/login"
+  if (!session && url !== req.url) {
     const url = new URL("/login", req.url)
     return NextResponse.redirect(url)
-  } else if (session && path === "/login") {
-    return NextResponse.redirect(new URL("/", req.url))
   }
   return NextResponse.next()
 }
