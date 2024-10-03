02d8fa0875cdd111950aaa22b6f9b6b7f1e7a7b1
variyarudrax <rudrax.variya@bacancy.com>
2024-10-03 13:36:58 +0530

    fix(next auth): next auth

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { getToken } from "next-auth/jwt"
 import { NextResponse } from "next/server"
 
-const secret = process.env.NEXTAUTH_SECRET || "default_secret"
+const secret = process.env.NEXTAUTH_SECRET || ""
 
 export async function middleware(req: any) {
   const token = await getToken({ req, secret })
